c316e279d7461e643fdc1367fa304ec86ca27d1f
Crazys-Corner <craazygaming1021@gmail.com>
2023-06-02 18:58:32 -0400

    added more setup options for branding

--- webhook.php
+++ webhook.php
@@ -1,21 +1,24 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $webhook = $_POST['webhook'];
+    $branding = $_POST['branding'];
 
     // Generate the PHP code
     $phpCode = <<<EOT
 <?php
 putenv("DISCORD_WEBHOOK_URL={$webhook}");
+putenv("BRANDING_TEXT={$branding}");
 EOT;
 
     // Save the generated code to a file
     $filename = 'config.php'; // Adjust the filename as needed
     file_put_contents($filename, $phpCode);
 
-    // Set the environmental variable for the current script execution
+    // Set the environmental variables for the current script execution
     putenv("DISCORD_WEBHOOK_URL={$webhook}");
+    putenv("BRANDING_TEXT={$branding}");
 
-    // Delete the setup script
+    // Delete the setup.html file
     unlink('setup.html');
 
     echo "Setup script completed. File created: $filename";
