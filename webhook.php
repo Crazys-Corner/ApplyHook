04d31b238596e1bdec128c015dac531e3614403f
Crazys-Corner <craazygaming1021@gmail.com>
2023-06-02 16:35:44 -0400

    Setup Files for env vars

---
+++ webhook.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $webhook = $_POST['webhook'];
+
+    // Generate the PHP code
+    $phpCode = <<<EOT
+<?php
+putenv("DISCORD_WEBHOOK_URL={$webhook}");
+EOT;
+
+    // Save the generated code to a file
+    $filename = 'config.php'; // Adjust the filename as needed
+    file_put_contents($filename, $phpCode);
+
+    // Set the environmental variable for the current script execution
+    putenv("DISCORD_WEBHOOK_URL={$webhook}");
+
+    // Delete the setup script
+    unlink(setup.html);
+
+    echo "Setup script completed. File created: $filename";
+}
+?>
